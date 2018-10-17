a693323c565ea95ccc9f0ba73a8f176b801df1cf
unknown <kaden.zipfel@hotmail.com>
2018-10-17 12:45:16 -0700

    Setup auth flow for dev and prod

---
+++ server/config/prod.js
@@ -0,0 +1,8 @@
+module.exports = {
+  google: {
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET
+  },
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY
+}
