958590c6844204516ec3ac9dc838880feb165ba4
unknown <kaden.zipfel@hotmail.com>
2018-11-14 12:13:52 -0800

    Complete webhook handling

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -4,10 +4,41 @@ const requireCredits = require('../middleware/requireCredits');
 const Survey = mongoose.model('surveys');
 const Mailer = require('../services/Mailer');
 const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
+const _ = require('lodash');
+const {Path} = require('path-parser');
+const {URL} = require('url');
 
 module.exports = app => {
-  app.get('/api/surveys/thanks', (req, res) => {
+  app.get('/api/surveys/:surveyId/:choice', (req, res) => {
     res.send('Thanks for your input!');
+  });
+
+  app.post('/api/surveys/webhooks', (req, res) => {
+    const p = new Path('/api/surveys/:surveyId/:choice');
+    _.chain(req.body)
+      .map(({url, email}) => {
+        const match = p.test(new URL(url).pathname);
+        if(match) {
+          return {email, surveyId: match.surveyId, choice: match.choice};
+        }
+      })
+      .compact()
+      .uniqBy('email', 'surveyId')
+      .each(({surveyId, email, choice}) => {
+        Survey.updateOne({
+          _id: surveyId,
+          recipients: {
+            $elemMatch: {email: email, responded: false}
+          }
+        }, {
+          $inc: {[choice]: 1},
+          $set: {'recipients.$.responded': true},
+          lastResponded: new Date()
+        }).exec();
+      })
+      .value();
+
+    res.send({});
   });
 
   app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
